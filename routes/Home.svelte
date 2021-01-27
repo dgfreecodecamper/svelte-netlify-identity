a882a7575877cf79f8ac5fe56636e221bd91332c
David <dgfreecodecamper@gmail.com>
2021-01-27 22:03:28 +0000

    login button added

--- routes/Home.svelte
+++ routes/Home.svelte
@@ -1,2 +1,11 @@
+<script>
+  const handleLogin = () => {
+    console.log("login clicked");
+    netlifyIdentity.open();
+  };
+</script>
+
 <h1>Home page</h1>
 <p>netlify.toml file added</p>
+
+<button on:click={handleLogin}>login</button>
